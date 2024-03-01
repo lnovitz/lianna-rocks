86e042285c56346083a1cbe11ab3129cea021c65
Lianna <liannanovitz@gmail.com>
2024-02-29 22:28:22 -0800

    use functional component instead of class

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,63 +1,54 @@
 import me from "../me.jpg";
 import "../App.css";
-import React, { Component } from "react";
+import React from "react";
 import useSound from "use-sound";
 import boopSfx from "../boop.mp3";
 
-class Home extends Component {
-  constructor(props) {
-    super(props);
+function Home() {
+  const [play, { stop }] = useSound(boopSfx, { volume: 0.5 });
+  const [isHovering, setIsHovering] = React.useState(false);
 
-    this.state = { active: false };
-    this.updateOnMouseEnter = this.updateOnMouseEnter.bind(this);
-    this.updateOnMouseLeave = this.updateOnMouseLeave.bind(this);
-  }
-
-  updateOnMouseEnter() {
-    this.setState({ active: true });
-  }
-
-  updateOnMouseLeave() {
-    this.setState({ active: false });
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img
-            src={me}
-            style={{
-              animation: ` ${
-                !this.state.active ? "App-logo-spin infinite 20s linear" : null
-              }`,
-              height: `40vmin`,
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src={me}
+          style={{
+            animation: ` ${
+              !isHovering ? "App-logo-spin infinite 20s linear" : null
+            }`,
+            height: `40vmin`,
+          }}
+          alt="logo"
+        />
+        <p>
+          <br></br>
+          <br></br>
+          <br></br>I'm a Python engineer (5+ years) who knows JavaScript.{" "}
+          <br></br>
+          Want the spinning to stop?{" "}
+          <a
+            className="App-link"
+            id="booking"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.cal.com/she-SWE/hello?duration=30"
+            onMouseEnter={() => {
+              setIsHovering(true);
+              play();
             }}
-            alt="logo"
-          />
-          <p>
-            <br></br>
-            <br></br>
-            <br></br>I'm a Python engineer (5+ years) who knows JavaScript.{" "}
-            <br></br>
-            Want the spinning to stop?{" "}
-            <a
-              className="App-link"
-              id="booking"
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.cal.com/she-SWE/hello?duration=30"
-              onMouseEnter={this.updateOnMouseEnter}
-              onMouseLeave={this.updateOnMouseLeave}
-            >
-              Hire me
-            </a>
-            !
-          </p>
-        </header>
-      </div>
-    );
-  }
+            onMouseLeave={() => {
+              setIsHovering(false);
+              stop();
+            }}
+          >
+            Hire me
+          </a>
+          !
+        </p>
+      </header>
+    </div>
+  );
 }
 
 export default Home;
